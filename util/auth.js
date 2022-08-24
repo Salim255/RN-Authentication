17e923fbc5669edc521980468c9216ad59fe1853
Salim <79255969+Salim255@users.noreply.github.com>
2022-08-24 18:13:53 +0200

    Extracting the Authentification Token

--- util/auth.js
+++ util/auth.js
@@ -11,13 +11,14 @@ async function authenticate(mode, email, password) {
     returnSecureToken: true,
   });
 
-  console.log(response);
+  const token = response.data.idToen;
+  return token;
 }
 
-export async function createUser(email, password) {
-  await authenticate("signUp", email, password);
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
 }
 
-export async function login(email, password) {
-  await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }

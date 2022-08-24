f69de4fdc1e7bfb31724fc9bcb6880810c4d59c4
Salim <79255969+Salim255@users.noreply.github.com>
2022-08-24 11:05:28 +0200

    Creating New Users

--- util/auth.js
+++ util/auth.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyD0uDb1sr7oqfgqegxqRl1zbLmrwSZNElo";
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     { email: email, password: password, returnSecureToken: true }
   );
+  //return response;
 }
